770704b747b6d622f725ca636fc7dfef7d745588
vinay.itapu <vinay.itapu@thomsonreuters.com>
2019-06-28 23:19:00 +0530

    Week1 homework task commit

---
+++ dist/models/Product.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Product = function Product() {
+  _classCallCheck(this, Product);
+
+  console.log("Product module");
+};
+
+exports["default"] = Product;
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import name from './config/config'; 
+import {
+    User,
+    Product
+} from './models/index';
+
+console.log(name);
+var user = new User();
+var product = new Product();

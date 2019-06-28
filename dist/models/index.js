2600d671220cebbac7f765bad74961f32a068c69
vinay.itapu <vinay.itapu@thomsonreuters.com>
2019-06-28 23:59:51 +0530

    Week1 home work commit

---
+++ dist/models/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Product", {
+  enumerable: true,
+  get: function get() {
+    return _Product["default"];
+  }
+});
+Object.defineProperty(exports, "User", {
+  enumerable: true,
+  get: function get() {
+    return _User["default"];
+  }
+});
+
+var _Product = _interopRequireDefault(require("./Product"));
+
+var _User = _interopRequireDefault(require("./User"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }

474dffe00291d2738e8d5a476a5ded1f4790c1c9
Vinay Itapu <Vinay_Itapu@epam.com>
2019-08-12 15:59:51 +0530

    Corrected condition for local strategy

--- src/express-app/strategies/local.js
+++ src/express-app/strategies/local.js
@@ -4,7 +4,7 @@ import * as config from "../config";
 export const localStrategy = () => {
   return new LocalStrategy((username, password, cb) => {
     const { username: configUsername, password: configPassword } = config.user;
-    if (username !== configUsername && password !== configPassword)
+    if (username !== configUsername || password !== configPassword)
       return cb(null, false);
     else return cb(null, config.user);
   });
